e318f7080b9970373d8fad3aea26d45dee3c6b4f
Casa <Casa@192.168.1.4>
2014-09-09 21:16:55 -0600

    Documentos

--- src/bstnode/Pruebas.java
+++ src/bstnode/Pruebas.java
@@ -10,21 +10,20 @@ public class Pruebas {
     public static void main(String[] args) {    
     
     BST tree = new BST();
-    tree.put( "f", "eff" );
-    tree.put( "c", "sea" );
-    tree.put( "a", "aye" );
-    tree.put( "e", "eee" );
-    tree.put( "i", "eye" );
+    tree.put( "facil", "eff" );
+    tree.put( "conjunto", "eee" );
+    tree.put( "aire", "aye" );
+    tree.put( "e", "sea" );
+    tree.put( "iglu", "eye" );
     tree.put( "h", "aitch" );
     tree.put( "z", "zed" );
  //   String str = tree.get("i"); // str will equal "eye"
  //   tree.put( "i", "eye updated" );
  //   str = tree.get( "i" ); // str will equal "eye updated"
 
-    System.out.println("Impresion como objeto = "+ tree.get( "c" ));//imprime como objecto
-    
+
     System.out.println("BUSCAR");
-    str = tree.get( "c" );      //objecto
+    str = tree.get( "e" );      //objecto
      
     nuevo = "sea";                                  // Palabra que se desea buscar
     String AsString = "" + nuevo;                   // Lo coloca como tostring
@@ -32,7 +31,8 @@ public class Pruebas {
     entero= AsString.compareTo(str.toString());     //como to string compara
     System.out.println(entero );                    // = 0 lo encontro
     System.out.println("Lo encontro = 0, sino =! 0");
-    
+    System.out.println("Impresion como objeto = "+ tree.get( "e" ));//imprime como objecto
+     
     System.out.println();
     System.out.println("INordered Strings:");
     BST.InorderPrint(tree.getRoot());
